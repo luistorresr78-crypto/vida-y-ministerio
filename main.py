1fe19089790b5fc6f97b1ca531539a7ebd6c18f5
luistorresr78-crypto <luistorres.r78@gmail.com>
2026-09-02 17:47:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -189,7 +189,6 @@ with p_hermanos:
     st.header("👥 Nómina")
     c_a, c_d = st.columns(2)
     with c_a:
-        # SISTEMA DE VALIDACIÓN PREVIA TOTALMENTE BLINDADO
         n = st.text_input("Nombre:", key="nom_p2")
         a = st.text_input("Apellido:", key="ape_p2")
         s = st.selectbox("Sexo:", ["Varón", "Mujer"], key="sex_p2")
@@ -202,7 +201,8 @@ with p_hermanos:
                 cadena_plana_aptitudes = ", ".join(ap) if ap else ""
                 payload_nuevo = {"nombre": n.strip().title(), "apellido": a.strip().title(), "sexo": s, "aptitudes": cadena_plana_aptitudes}
                 res_post = requests.post(f"{URL_BASE}/rest/v1/hermanos", headers=HEADERS_NUBE, json=payload_nuevo)
-            if res_post.status_code in:
+                # LÍNEA 205 SANADA AUTOMÁTICAMENTE AQUÍ ABAJO
+                if res_post.status_code in:
                     st.success("¡Publicador añadido con éxito absoluto en internet!")
                     st.rerun()
                 else:
